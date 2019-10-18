982d39091f7a84b45a3e8db9816902e3be8459d5
ethanmanco <ethanmanco@gmail.com>
2019-10-17 20:38:35 -0500

    Point creation added with MarkerLayerOptions

--- lib/main.dart
+++ lib/main.dart
@@ -23,7 +23,7 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
-        appBar: new AppBar(title: new Text('Leaflet Maps')),
+        appBar: new AppBar(title: new Text('Yeet Seek')),
         body: new FlutterMap(
             options: new MapOptions(
                 center: new LatLng(35.22, -101.83), minZoom: 10.0),
@@ -35,7 +35,23 @@ class _MyHomePageState extends State<MyHomePage> {
                     'accessToken':
                     '',
                     'id': ''
-                  })
+                  }),
+              new MarkerLayerOptions(markers: [
+                new Marker(
+                    width: 45.0,
+                    height: 45.0,
+                    point: new LatLng(35.215, -101.825),
+                    builder: (context) => new Container(
+                      child: IconButton(
+                        icon: Icon(Icons.location_on),
+                        color: Colors.blue,
+                        iconSize: 45.0,
+                        onPressed: () {
+                          print('Marker tapped');
+                        },
+                      ),
+                    ))
+              ])
             ]));
   }
 }
