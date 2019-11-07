278c0230bf4d2376788bcf57823fd498cc7c386b
ethanmanco <ethanmanco@gmail.com>
2019-11-07 11:49:03 -0600

    Examples for creating, updating, and deleting markers added

--- lib/ServerConnection.dart
+++ lib/ServerConnection.dart
@@ -8,8 +8,29 @@ main(List<String> arguments) async {
 
   DbCollection coll = db.collection('markers');
 
+  // List markers
   var markers = await coll.find().toList();
   print(markers);
+
+  // Create markers
+  /*await coll.save({
+    "id": 2,
+    "LatLng": "35.23, -101.87"
+  });
+  print('Marker saved!');
+
+  // Update markers
+  await coll.update(await coll.findOne(where.eq('id', 2)), {
+    r'$set': {
+      'LatLng': '35.20, -101.89'
+    }
+  });
+  print('Updated person: ');
+  print(await coll.findOne(where.eq('id', 2)));
+
+  // Delete markers
+  await coll.remove(await coll.findOne(where.eq('id', 2)));
+  print(await coll.findOne(where.eq('id', 2)));*/
 
   await db.close();
 }
